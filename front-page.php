8eb298d9b4118c30538ce4cca9122a3e155a26a9
Doshro <ruku.bhattarai@gmail.com>
2022-08-29 15:24:29 -0700

    Hero section added

--- front-page.php
+++ front-page.php
@@ -12,5 +12,7 @@
 ?>
 
 <?php get_header(); ?>
+<?php get_template_part('template-parts/hero-section'); ?>
+
 
 <?php get_footer(); ?>
---
+++ template-parts/hero-section.php
@@ -0,0 +1,6 @@
+<div class="hero-section">
+
+    <h1>Water Scarcity</h1>
+    <h3>Slogan goes here</h2>
+
+</div>
