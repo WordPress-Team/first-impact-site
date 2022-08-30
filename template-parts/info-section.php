2d3698c560dec1b35b6ecf04b033552b8f5e8d21
MilesAngels <mangels@alphaworks.tech>
2022-08-30 10:20:13 -0700

    updated responsiveness of info section

--- template-parts/info-section.php
+++ template-parts/info-section.php
@@ -3,10 +3,12 @@
         <h2 class="title">
             What is Water Scarcity?
         </h2>
-        <img class="info-image" src="http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg" alt="River drying up">
-        <p class="content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae fuga rem totam labore, repudiandae doloremque quae dolore soluta quas tenetur eos odio corrupti ex sint officiis accusantium eius velit.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae fuga rem totam labore, repudiandae doloremque quae dolore soluta quas tenetur eos odio corrupti ex sint officiis accusantium eius velit.
-        </p>
+        <div class="info-container">
+            <img class="info-image" src="http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg" alt="River drying up">
+            <p class="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae fuga rem totam labore, repudiandae doloremque quae dolore soluta quas tenetur eos odio corrupti ex sint officiis accusantium eius velit.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae fuga rem totam labore, repudiandae doloremque quae dolore soluta quas tenetur eos odio corrupti ex sint officiis accusantium eius velit.
+            </p>
+        </div>
     </article>
 </section>
