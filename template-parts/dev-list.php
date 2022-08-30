29ed1611470c6ebda2e7c2f876d31a1e76ae7bc5
Doshro <ruku.bhattarai@gmail.com>
2022-08-29 17:01:38 -0700

    initial commit

---
+++ template-parts/dev-list.php
@@ -0,0 +1,24 @@
+<section class= "dev-list">
+    <h3>Want To Meet The Team?</h1>
+    <div class= "water-team">
+        <div class="miles">
+            <img class="miles-img" src="" alt="Miles's image">
+            <p>Miles Angles</p>
+            <p>WordPress Apprentice at Alphaworks</p>
+        </div>
+
+        <div class="george">
+            <img class="george-img" src="" alt="George's image">
+            <p>George Meadors</p>
+            <p>WordPress Apprentice at Alphaworks</p>
+        </div>
+
+        <div class="ruku">
+            <img class="ruku-img" src="" alt="Ruku's image">
+            <p>Ruku Bhattarai</p>
+            <p>WordPress Apprentice at Alphaworks</p>
+        </div>
+
+    </div>
+
+</section>
