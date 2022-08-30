ed7bd8148287bd08d7138839f78b43d7f2ff52f7
MilesAngels <mangels@alphaworks.tech>
2022-08-30 09:34:16 -0700

    creating base structure for info section

--- template-parts/info-section.php
+++ template-parts/info-section.php
@@ -3,8 +3,9 @@
         <h2 class="title">
             What is Water Scarcity?
         </h2>
-        <img class="info-image" src="#" alt="River drying up">
+        <img class="info-image" src="http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg" alt="River drying up">
         <p class="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae fuga rem totam labore, repudiandae doloremque quae dolore soluta quas tenetur eos odio corrupti ex sint officiis accusantium eius velit.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae fuga rem totam labore, repudiandae doloremque quae dolore soluta quas tenetur eos odio corrupti ex sint officiis accusantium eius velit.
         </p>
     </article>
