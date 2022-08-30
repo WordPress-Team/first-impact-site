e8001f46fbd9942f663c4c7f30a55928d75fba6f
Doshro <ruku.bhattarai@gmail.com>
2022-08-30 15:46:21 -0700

    dev list added

--- template-parts/dev-list.php
+++ template-parts/dev-list.php
@@ -1,21 +1,21 @@
 <section class= "dev-list">
-    <h3>Want To Meet The Team?</h1>
+    <h2>Want To Meet The Team?</h2>
     <div class= "water-team">
-        <div class="miles">
-            <img class="miles-img" src="" alt="Miles's image">
-            <p>Miles Angles</p>
+        <div class="devs">
+            <img class="dev-img" src="http://firstimpactsite.local/wp-content/uploads/2022/08/Miles.png" alt="Miles's image">
+            <h3>Miles Angles</h3>
             <p>WordPress Apprentice at Alphaworks</p>
         </div>
 
-        <div class="george">
-            <img class="george-img" src="" alt="George's image">
-            <p>George Meadors</p>
+        <div class="devs">
+            <img class="dev-img" src="http://firstimpactsite.local/wp-content/uploads/2022/08/George-scaled.jpg" alt="George's image">
+            <h3>George Meadors</h3>
             <p>WordPress Apprentice at Alphaworks</p>
         </div>
 
-        <div class="ruku">
-            <img class="ruku-img" src="" alt="Ruku's image">
-            <p>Ruku Bhattarai</p>
+        <div class="devs">
+            <img class="dev-img" src="http://firstimpactsite.local/wp-content/uploads/2022/08/ruku.png" alt="Ruku's image">
+            <h3>Ruku Bhattarai</h3>
             <p>WordPress Apprentice at Alphaworks</p>
         </div>
 
