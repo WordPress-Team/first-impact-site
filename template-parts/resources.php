d3dde99fe7b54957e2ac9ac8fdabb8180b56aff9
MilesAngels <mangels@alphaworks.tech>
2022-08-31 10:17:09 -0700

    resource section responsive

--- template-parts/resources.php
+++ template-parts/resources.php
@@ -1,47 +1,52 @@
-<section class="gallery">
-    <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
-        <div class="card-content">
-            <h2 class="card-title">Hello</h2>
-            <p>Hello</p>
-            <a href="#" target="_blank">
-                <button class="redirect-button">Learn More</button>
-            </a>
-        </div>
-    </div>
-    <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
-        <div class="card-content">
-            <h2 class="card-title">Hello</h2>
-            <p>Hello</p>
-            <a href="#" target="_blank">
-                <button class="redirect-button">Learn More</button>
-            </a>
-        </div>
-    </div>
-    <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
-        <div class="card-content">
-            <h2 class="card-title">Hello</h2>
-            <p>Hello</p>
-            <a href="#" target="_blank">
-                <button class="redirect-button">Learn More</button>
-            </a>
-        </div>
-    </div>
-    <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
-        <div class="card-content">
-            <h2 class="card-title">Hello</h2>
-            <p>Hello</p>
-            <a href="#" target="_blank">
-                <button class="redirect-button">Learn More</button>
-            </a>
-        </div>
-    </div>
-    <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
-        <div class="card-content">
-            <h2 class="card-title">Hello</h2>
-            <p>Hello</p>
-            <a href="#" target="_blank">
-                <button class="redirect-button">Learn More</button>
-            </a>
-        </div>
-    </div>
+<section class="info-section">
+    <article class="intro">
+        <h2 class="title">Resources</h2>
+        <section class="gallery">
+            <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
+                <div class="card-content">
+                    <h2 class="card-title">Hello</h2>
+                    <p>Hello</p>
+                    <a href="#" target="_blank">
+                        <button class="redirect-button">Learn More</button>
+                    </a>
+                </div>
+            </div>
+            <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
+                <div class="card-content">
+                    <h2 class="card-title">Hello</h2>
+                    <p>Hello</p>
+                    <a href="#" target="_blank">
+                        <button class="redirect-button">Learn More</button>
+                    </a>
+                </div>
+            </div>
+            <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
+                <div class="card-content">
+                    <h2 class="card-title">Hello</h2>
+                    <p>Hello</p>
+                    <a href="#" target="_blank">
+                        <button class="redirect-button">Learn More</button>
+                    </a>
+                </div>
+            </div>
+            <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
+                <div class="card-content">
+                    <h2 class="card-title">Hello</h2>
+                    <p>Hello</p>
+                    <a href="#" target="_blank">
+                        <button class="redirect-button">Learn More</button>
+                    </a>
+                </div>
+            </div>
+            <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
+                <div class="card-content">
+                    <h2 class="card-title">Hello</h2>
+                    <p>Hello</p>
+                    <a href="#" target="_blank">
+                        <button class="redirect-button">Learn More</button>
+                    </a>
+                </div>
+            </div>
+        </section>
+    </article>
 </section>
