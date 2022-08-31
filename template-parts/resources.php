e9ff9d193ddc647e8632d7abce606bcc6af027ea
MilesAngels <mangels@alphaworks.tech>
2022-08-31 13:49:58 -0700

    finalizing cards

--- template-parts/resources.php
+++ template-parts/resources.php
@@ -38,15 +38,6 @@
                     </a>
                 </div>
             </div>
-            <div class="cards" style="background:url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); ">
-                <div class="card-content">
-                    <h2 class="card-title">Hello</h2>
-                    <p>Hello</p>
-                    <a href="#" target="_blank">
-                        <button class="redirect-button">Learn More</button>
-                    </a>
-                </div>
-            </div>
         </section>
     </article>
 </section>
