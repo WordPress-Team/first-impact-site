a3325d999db3736b032f1e38c7e9158caf0bd63a
MilesAngels <mangels@alphaworks.tech>
2022-08-31 09:09:17 -0700

    adding cards

--- template-parts/resources.php
+++ template-parts/resources.php
@@ -2,11 +2,21 @@
     <h2 class="resource-title">
         Resources
     </h2>
-    <div class="card">
-        <p class="card-title">Title</p>
-        <p class="card-description">Description</p>
-        <div class="button-container">
-            <button class="card-button">Learn More</button>
-        </div>
+    <div class="">
+        <ul class="card-wrapper">
+            <li class="card">
+                <img class="card-image" src="http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg" alt="">
+                <h3 class="card-title">Title</h3>
+                <p>Description</p>
+                <button class="card-button">Learn More</button>
+            </li>
+            <li class="card" style="background-image: url('http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg'); width: 100%;
+    object-fit: contain; aspect-ratio: 4/3;">
+                <!-- <img class="card-image" src="http://firstimpactsite.local/wp-content/uploads/2022/08/river-drying-up-scaled.jpg" alt=""> -->
+                <h3 class="card-title">Title</h3>
+                <p>Description</p>
+                <button class="card-button">Learn More</button>
+            </li>
+        </ul>
     </div>
 </section>
