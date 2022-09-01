c915289de365599a5394a6209a6f8e401de38094
MilesAngels <mangels@alphaworks.tech>
2022-09-01 16:19:39 -0700

    removed space on top of hero section

--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -1,6 +1,4 @@
-<div class="hero-section">
-
+<div class="hero-section" style="background-image: url('http://firstimpactsite.local/wp-content/uploads/2022/09/drinking-dirty-water.jpg');">
     <h1>Water Scarcity</h1>
     <h3>Slogan goes here</h3>
-
 </div>
