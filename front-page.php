56d7adb58eb1581143be009c231ef639387cad02
MilesAngels <mangels@alphaworks.tech>
2022-09-02 06:42:33 -0700

    update

--- front-page.php
+++ front-page.php
@@ -13,10 +13,7 @@
 
 <?php get_header(); ?>
 <?php get_template_part('template-parts/hero-section'); ?>
-
-
 <?php get_template_part('template-parts/info-section'); ?>
-
 <?php get_template_part('template-parts/resources'); ?>
 
 <?php get_footer(); ?>
